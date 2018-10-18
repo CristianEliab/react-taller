69bc2c40d07304a1457f496ada8218e9db99c288
Cristian Eliab Rodriguez Chuquen <1234189967@academico.icesi.edu.co>
2018-10-18 00:02:38 -0500

    Updates

---
+++ precache-manifest.9c0cf42d92f3b7e057f819aab24a8e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97e5c4a304cf8697bcb58693aee5c481",
+    "url": "/react-taller/static/media/ban222.97e5c4a3.jpg"
+  },
+  {
+    "revision": "eac06ff29183552446f5",
+    "url": "/react-taller/static/js/runtime~main.eac06ff2.js"
+  },
+  {
+    "revision": "b035250a8d8b5c2deb50",
+    "url": "/react-taller/static/js/main.b035250a.chunk.js"
+  },
+  {
+    "revision": "b0b62f0858f72a489cb8",
+    "url": "/react-taller/static/js/1.b0b62f08.chunk.js"
+  },
+  {
+    "revision": "b035250a8d8b5c2deb50",
+    "url": "/react-taller/static/css/main.1638d183.chunk.css"
+  },
+  {
+    "revision": "c121146ccca26b048205e6c4b7ab8077",
+    "url": "/react-taller/index.html"
+  }
+];
